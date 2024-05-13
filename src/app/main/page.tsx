01c5483c89acf880a01f4b7e979ad2289c3077fc
Harish Malakar <harishmalakar19@gmail.com>
2024-05-13 14:35:48 +0530

    shadcn added

---
+++ src/app/main/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Input } from "@/components/ui/input"
+
+
+function page() {
+  return (
+    <div className=''>
+    <div><Input placeholder='username'/></div>
+    <div><Input placeholder='email'/></div>
+    <div><Input placeholder='password'/></div>
+    </div>
+  )
+}
+
+export default page
