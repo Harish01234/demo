cbc0b1951859c628aeaf37d571ed3dac234e5ae4
Harish Malakar <harishmalakar19@gmail.com>
2024-05-13 23:12:31 +0530

    addexercise api added

---
+++ src/app/api/addexercise/route.tsx
@@ -0,0 +1,44 @@
+import connectDb from '@/lib/connectDb'
+import {User} from '@/model/User'
+import { error } from "console";
+import {NextRequest,NextResponse} from "next/server"
+import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+connectDb()
+export async function POST(request:NextRequest) {
+
+    const reqBody=await request.json()
+    const {username,name,reps,weight}=reqBody
+    try {
+        const user = await User.findOne({ username }).exec();
+
+        
+    if (!user) {
+        return Response.json(
+          { message: 'User not found', success: false },
+          { status: 404 }
+        );
+      }
+
+      const newExercise = { name,reps,weight };
+      user.exercises.push(newExercise)
+      await user.save();
+      
+    return Response.json(
+        { message: 'exercise added successfully', success: true },
+        { status: 201 }
+      );
+
+    } catch (error) {
+        
+        console.error('Error adding message:', error);
+        return Response.json(
+          { message: 'Internal server error', success: false },
+          { status: 500 }
+        );
+    }
+
+
+
+}
